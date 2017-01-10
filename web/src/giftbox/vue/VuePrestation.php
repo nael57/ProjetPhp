e69a6987f2fed9e7926c8062e19c60dfaf3426e5
Nael Bouzaza <nael.bouzaza5@etu.univ-lorraine.fr>
2017-01-10 10:58:16 +0100

    modification des footer

--- web/src/giftbox/vue/VuePrestation.php
+++ web/src/giftbox/vue/VuePrestation.php
@@ -122,76 +122,57 @@ class VuePrestation {
     </div>
 
 
+ <footer id="fh5co-footer" role="contentinfo">
+        <div class="container">
+            <div class="row row-pb-md">
+                <div class="col-md-3 fh5co-widget">
+                    <h4>À propos</h4>
+                    <p>GiftBox est une entreprise purement fictive réalisée dans le cadre du projet de notre 3ème semestre de DUT Informatique, toute ressemblance avec une entreprise existante nest pas voulue.</p>
+                </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                    <h4>Catalogue</h4>
+                    <ul class="fh5co-footer-links">
+                        '.$this->affich_liste_cat().'
+                    </ul>
+                </div>
 
-    <footer id="fh5co-footer" role="contentinfo">
-    <div class="container">
-    <div class="row row-pb-md">
-    <div class="col-md-3 fh5co-widget">
-    <h4>About Learning</h4>
-    <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
-    </div>
-    <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-    <h4>Learning</h4>
-    <ul class="fh5co-footer-links">
-    <li><a href="#">Course</a></li>
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">Contact</a></li>
-    <li><a href="#">Terms</a></li>
-    <li><a href="#">Meetups</a></li>
-    </ul>
-    </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                    <h4>Laventure GiftBox</h4>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="#">Le concept</a></li>
+                        <li><a href="#">Qui sommes nous</a></li>
+                    </ul>
+                </div>
 
-    <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-    <h4>Learn &amp; Grow</h4>
-    <ul class="fh5co-footer-links">
-    <li><a href="#">Blog</a></li>
-    <li><a href="#">Privacy</a></li>
-    <li><a href="#">Testimonials</a></li>
-    <li><a href="#">Handbook</a></li>
-    <li><a href="#">Held Desk</a></li>
-    </ul>
-    </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                    <h4>Nos partenaires</h4>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="#">IUT Charlemagne</a></li>
+                        <li><a href="#">Cours en PHP de Monsieur B.</a></li>
+                        <li><a href="#">OpenClassroom</a></li>
+                        <li><a href="#">Youtube</a></li>
+                    </ul>
+                </div>
 
-    <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-    <h4>Engage us</h4>
-    <ul class="fh5co-footer-links">
-    <li><a href="#">Marketing</a></li>
-    <li><a href="#">Visual Assistant</a></li>
-    <li><a href="#">System Analysis</a></li>
-    <li><a href="#">Advertise</a></li>
-    </ul>
-    </div>
+                <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+                    <h4>Connexion</h4>
+                    <ul class="fh5co-footer-links">
+                        <li><a href="#">Se connecter</a></li>
+                        <li><a href="#">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            </div>
 
-    <div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
-    <h4>Legal</h4>
-    <ul class="fh5co-footer-links">
-    <li><a href="#">Find Designers</a></li>
-    <li><a href="#">Find Developers</a></li>
-    <li><a href="#">Teams</a></li>
-    <li><a href="#">Advertise</a></li>
-    <li><a href="#">API</a></li>
-    </ul>
-    </div>
-    </div>
+            <div class="row copyright">
+                <div class="col-md-12 text-center">
+                    <p>
+                        <small class="block">&copy; 2016 Nael Léo Tolga Pierre Florian <strong>Groupe S3B</strong></small> 
+                    </p>
 
-    <div class="row copyright">
-    <div class="col-md-12 text-center">
-    <p>
-    <small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-    <small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
-    </p>
-    <p>
-    <ul class="fh5co-social-icons">
-    <li><a href="#"><i class="icon-twitter"></i></a></li>
-    <li><a href="#"><i class="icon-facebook"></i></a></li>
-    <li><a href="#"><i class="icon-linkedin"></i></a></li>
-    <li><a href="#"><i class="icon-dribbble"></i></a></li>
-    </ul>
-    </p>
-    </div>
-    </div>
+                </div>
+            </div>
 
-    </div>
+        </div>
     </footer>
     </div>
 
