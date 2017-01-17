fde08432c2a6760ac94bb8dda4dae2a1f049757a
Nael Bouzaza <nael.bouzaza5@etu.univ-lorraine.fr>
2017-01-17 14:38:40 +0100

    prestations les plus populaires et page index

--- web/src/giftbox/vue/VuePrestation.php
+++ web/src/giftbox/vue/VuePrestation.php
@@ -5,7 +5,6 @@ namespace giftbox\vue;
 use giftbox\models\Categorie as Categorie;
 use giftbox\models\Prestation as Prestation;
 use giftbox\models\Contient as Contient;
-use giftbox\models\Vote as Vote;
 
 
 //Classe vue pour le catalogue
@@ -43,7 +42,7 @@ class VuePrestation {
     }
 
     public function affich_moyenne(){
-        $vote = Vote::where('id_vote','=',$this->id)->first();
+        $vote = Prestation::where('id','=',$this->id)->first();
         return $vote['moyenne'];
     }
 
